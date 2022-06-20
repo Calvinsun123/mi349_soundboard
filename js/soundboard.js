7b4170e217f4104eba93afc4caf722da8e1c239c
Calvinsun123 <105680509+Calvinsun123@users.noreply.github.com>
2022-06-20 00:45:15 -0400

    Updated Java

---
+++ js/soundboard.js
@@ -0,0 +1,24 @@
+var gusy = document.querySelector(".gusy");
+var gu = document.querySelector(".gu");
+
+var dtqsy = document.querySelector(".dtqsy");
+var dtq = document.querySelector(".dtq");
+
+var gqsy = document.querySelector(".gqsy");
+var gq = document.querySelector(".gq");
+
+var jtsy = document.querySelector(".jtsy");
+var jt = document.querySelector(".jt");
+
+gu.onclick = function () {
+    gusy.play();
+};
+dtq.onclick = function () {
+    dtqsy.play();
+};
+gq.onclick = function () {
+    gqsy.play();
+};
+jt.onclick = function () {
+    jtsy.play();
+};
